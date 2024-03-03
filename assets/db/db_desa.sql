59f446b5486ac62fc65cd60bd943deb77c5a242d
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-03-03 21:07:01 +0700

    db_desa

---
+++ assets/db/db_desa.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 03 Mar 2024 pada 15.05
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_desa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penduduk`
+--
+
+CREATE TABLE `penduduk` (
+  `id` int(11) NOT NULL,
+  `nik` int(32) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `nama` varchar(36) NOT NULL,
+  `tempat_lahir` varchar(36) NOT NULL,
+  `tgl_lahir` varchar(36) NOT NULL,
+  `agama` varchar(36) NOT NULL,
+  `s_nikah` varchar(36) NOT NULL,
+  `pendidikan` varchar(36) NOT NULL,
+  `pekerjaan` varchar(36) NOT NULL,
+  `kk` int(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `username` varchar(36) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `username`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'Admin 1', 'admin', '$2y$10$WPi9x6rAEtz93yNgwzLJ0.G9rw7fCmhma0Cp/wIkODiu6rmCtrO3i', '2024-03-03 11:13:32', '2024-03-03 11:13:32');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `penduduk`
+--
+ALTER TABLE `penduduk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `penduduk`
+--
+ALTER TABLE `penduduk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

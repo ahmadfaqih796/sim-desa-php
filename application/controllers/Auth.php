a74945d273664e875a7f9826d94790387f7ef48b
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-03-03 17:59:28 +0700

    login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -10,7 +10,7 @@ class Auth extends CI_Controller
    }
    public function index()
    {
-      $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+      $this->form_validation->set_rules('username', 'Username', 'trim|required');
       $this->form_validation->set_rules('password', 'Password', 'trim|required');
       if ($this->form_validation->run() == false) {
          $data['title'] = 'Login';
@@ -24,9 +24,9 @@ class Auth extends CI_Controller
 
    private function _login()
    {
-      $email = $this->input->post('email');
+      $username = $this->input->post('username');
       $password = $this->input->post('password');
-      $user = $this->db->get_where('users', ['email' => $email])->row_array();
+      $user = $this->db->get_where('users', ['username' => $username])->row_array();
       // cek usernya ada
       if ($user) {
          // jika user aktif
@@ -35,7 +35,7 @@ class Auth extends CI_Controller
             if (password_verify($password, $user['password'])) {
                $data = [
                   'user_id' => $user['id'],
-                  'email' => $user['email'],
+                  'username' => $user['username'],
                   'role_id' => $user['role_id']
                ];
                $this->session->set_userdata($data);
@@ -56,18 +56,18 @@ class Auth extends CI_Controller
                redirect('auth');
             }
          } else {
-            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> This email has not been activated! </div>');
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> This username has not been activated! </div>');
             redirect('auth');
          }
       } else {
-         $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Email is not registered! </div>');
+         $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Username is not registered! </div>');
          redirect('auth');
       }
    }
 
    public function logout()
    {
-      $this->session->unset_userdata('email');
+      $this->session->unset_userdata('username');
       $this->session->unset_userdata('role_id');
       $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> You have been logged out! </div>');
       redirect('auth');
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -9,17 +9,19 @@
                      <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
                         <img src="<?= base_url('assets/') ?>images/logos/dark-logo.svg" width="180" alt="">
                      </a>
-                     <p class="text-center">Your Social Campaigns</p>
-                     <form>
+                     <p class="text-center">Login Admin</p>
+                     <?= $this->session->flashdata('message'); ?>
+                     <form action="<?= base_url('auth'); ?>" method="post">
                         <div class="mb-3">
-                           <label for="exampleInputEmail1" class="form-label">Username</label>
-                           <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                           <label for="username" class="form-label">Username</label>
+                           <input type="text" class="form-control" id="username" value="<?= set_value('username'); ?>">
+                           <?= form_error('username', '<small class="text-danger pl-3">', '</small>'); ?>
                         </div>
                         <div class="mb-4">
-                           <label for="exampleInputPassword1" class="form-label">Password</label>
-                           <input type="password" class="form-control" id="exampleInputPassword1">
+                           <label for="password" class="form-label">Password</label>
+                           <input type="password" class="form-control" id="password" value="<?= set_value('password'); ?>">
                         </div>
-                        <div class="d-flex align-items-center justify-content-between mb-4">
+                        <!-- <div class="d-flex align-items-center justify-content-between mb-4">
                            <div class="form-check">
                               <input class="form-check-input primary" type="checkbox" value="" id="flexCheckChecked" checked>
                               <label class="form-check-label text-dark" for="flexCheckChecked">
@@ -27,12 +29,12 @@
                               </label>
                            </div>
                            <a class="text-primary fw-bold" href="./index.html">Forgot Password ?</a>
-                        </div>
-                        <a href="./index.html" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign In</a>
-                        <div class="d-flex align-items-center justify-content-center">
+                        </div> -->
+                        <button type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign In</button>
+                        <!-- <div class="d-flex align-items-center justify-content-center">
                            <p class="fs-4 mb-0 fw-bold">New to Modernize?</p>
                            <a class="text-primary fw-bold ms-2" href="./authentication-register.html">Create an account</a>
-                        </div>
+                        </div> -->
                      </form>
                   </div>
                </div>

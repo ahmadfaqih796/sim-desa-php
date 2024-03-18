eab7e01d9afdac0b489235a1e234807a02a24518
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-03-18 14:06:55 +0700

    login auth

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -10,16 +10,31 @@ class Auth extends CI_Controller
    }
    public function index()
    {
-      $this->_validation("admin");
+      $this->_validation("user");
       if ($this->form_validation->run() == false) {
          $data['title'] = 'Login';
          $this->load->view('templates/auth/header', $data);
          $this->load->view('auth/login');
          $this->load->view('templates/auth/footer');
       } else {
-         $this->_login();
+         $this->_login('user');
       }
    }
+
+   public function admin()
+   {
+      $this->_validation("admin");
+      if ($this->form_validation->run() == false) {
+         $data['title'] = 'Login';
+         $this->load->view('templates/auth/header', $data);
+         $this->load->view('auth/login_admin');
+         $this->load->view('templates/auth/footer');
+      } else {
+         $this->_login('admin');
+      }
+   }
+
+
 
    private function _validation($role = null)
    {
@@ -28,30 +43,62 @@ class Auth extends CI_Controller
             'required' => 'Username harus diisi'
          ]);
       }
+      if ($role == "user") {
+         $this->form_validation->set_rules('nik', 'NIK', 'trim|required', [
+            'required' => 'NIK harus diisi'
+         ]);
+      }
       $this->form_validation->set_rules('password', 'Password', 'trim|required', [
          'required' => 'Password harus diisi'
       ]);
    }
 
-   private function _login()
+   private function _login($role = null)
    {
       $username = $this->input->post('username');
+      $nik = $this->input->post('nik');
       $password = $this->input->post('password');
-      $user = $this->db->get_where('users', ['username' => $username])->row_array();
-      if ($user) {
-         if (password_verify($password, $user['password'])) {
-            $data = [
-               'user_id' => $user['id'],
-               'username' => $user['username'],
-               'role_id' => 1
-            ];
-            $this->session->set_userdata($data);
-            $this->notification->notify_success('management/dashboard', 'Anda berhasil login');
-         } else {
-            $this->notification->notify_error('auth', 'Password yang anda masukkan salah');
-         }
-      } else {
-         $this->notification->notify_error('auth', 'Username tidak terdaftar');
+
+      switch ($role) {
+         case 'admin':
+            $user = $this->db->get_where('users', ['username' => $username])->row_array();
+            if ($user) {
+               if (password_verify($password, $user['password'])) {
+                  $data = [
+                     'user_id' => $user['id'],
+                     'username' => $user['username'],
+                     'role_id' => 1
+                  ];
+                  $this->session->set_userdata($data);
+                  $this->notification->notify_success('home/dashboard', 'Anda berhasil login');
+               } else {
+                  $this->notification->notify_error('auth', 'Password yang anda masukkan salah');
+               }
+            } else {
+               $this->notification->notify_error('auth', 'Username tidak terdaftar');
+            };
+            break;
+         case 'user':
+            $user = $this->db->get_where('penduduk', ['nik' => $nik])->row_array();
+            if ($user) {
+               if (password_verify($password, $user['password'])) {
+                  $data = [
+                     'user_id' => $user['id'],
+                     'nik' => $user['nik'],
+                     'role_id' => 1
+                  ];
+                  $this->session->set_userdata($data);
+                  $this->notification->notify_success('home/dashboard', 'Anda berhasil login');
+               } else {
+                  $this->notification->notify_error('auth', 'Password yang anda masukkan salah');
+               }
+            } else {
+               $this->notification->notify_error('auth', 'Username tidak terdaftar');
+            };
+            break;
+         default:
+            $role_id = 0;
+            break;
       }
    }
 
--- application/views/auth/login_admin.php
+++ application/views/auth/login_admin.php
@@ -9,33 +9,20 @@
                      <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
                         <img src="<?= base_url('assets/') ?>images/logos/dark-logo.svg" width="180" alt="">
                      </a>
-                     <p class="text-center">Login User</p>
+                     <p class="text-center">Login Admin</p>
                      <?= $this->session->flashdata('message'); ?>
-                     <form action="<?= base_url('auth'); ?>" method="post">
+                     <form action="<?= base_url('auth/admin'); ?>" method="post">
                         <div class="mb-3">
-                           <label for="nik" class="form-label">NIK</label>
-                           <input type="text" class="form-control" name="nik" id="nik" value="<?= set_value('nik'); ?>">
-                           <?= form_error('nik', '<small class="text-danger pl-3">', '</small>'); ?>
+                           <label for="username" class="form-label">Username</label>
+                           <input type="text" class="form-control" name="username" id="username" value="<?= set_value('username'); ?>">
+                           <?= form_error('username', '<small class="text-danger pl-3">', '</small>'); ?>
                         </div>
                         <div class="mb-4">
                            <label for="password" class="form-label">Password</label>
                            <input type="password" class="form-control" name="password" id="password" value="<?= set_value('password'); ?>">
                            <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                         </div>
-                        <!-- <div class="d-flex align-items-center justify-content-between mb-4">
-                           <div class="form-check">
-                              <input class="form-check-input primary" type="checkbox" value="" id="flexCheckChecked" checked>
-                              <label class="form-check-label text-dark" for="flexCheckChecked">
-                                 Remeber this Device
-                              </label>
-                           </div>
-                           <a class="text-primary fw-bold" href="./index.html">Forgot Password ?</a>
-                        </div> -->
                         <button type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign In</button>
-                        <!-- <div class="d-flex align-items-center justify-content-center">
-                           <p class="fs-4 mb-0 fw-bold">New to Modernize?</p>
-                           <a class="text-primary fw-bold ms-2" href="./authentication-register.html">Create an account</a>
-                        </div> -->
                      </form>
                   </div>
                </div>
